0e756554cbbf51c522e75dd110222cb246f5c269
Eric Patey <eric.patey@sonos.com>
2022-08-06 17:15:52 -0400

    temp disable markdown

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -2,6 +2,7 @@ import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import ReactMarkdown from "react-markdown";
+import { LiProps, UnorderedListProps } from "react-markdown/lib/ast-to-react";
 import invariant from "tiny-invariant";
 
 import type { Post } from "~/models/post.server";
@@ -18,14 +19,19 @@ export const loader: LoaderFunction = async ({ params }) => {
   return json<LoaderData>({ post });
 };
 
+export const components = {
+  ul: ({ children }: UnorderedListProps) => <ul className="list-disc ml-4 mt-4">{children}</ul>,
+  // li: ({ children }: LiProps) => <li className=" mt-1">{children}</li>,
+}
+
 export default function PostSlug() {
   const { post } = useLoaderData<LoaderData>();
 
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
-      <ReactMarkdown>*React-Markdown* is **Awesome**</ReactMarkdown>
-      <ReactMarkdown children={post.markdown} />
+      {post.markdown}
+      {/* <ReactMarkdown children={post.markdown} components={components} /> */}
     </main>
   );
 }
